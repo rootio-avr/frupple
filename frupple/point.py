b806ee2c28f293372e117a6149c06f81544b22a3
Dmitry Rubinstein <dmitry.rubenstein@slim.ai>
2025-07-23 17:12:59 +0300

    Version 3.0.0

--- frupple/point.py
+++ frupple/point.py
@@ -3,6 +3,10 @@ Point utility class.
 """
 
 import math
+from typing import Union, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .line import Line
 
 
 class Point:
@@ -18,16 +22,22 @@ class Point:
         self.x = x
         self.y = y
     
-    def distance_to(self, other: 'Point') -> float:
-        """Calculate distance to another point.
+    def distance_to(self, other: Union['Point', 'Line']) -> float:
+        """Calculate distance to another point or line.
         
         Args:
-            other: Another Point object
+            other: Another Point object or Line object
             
         Returns:
-            The Euclidean distance between the points
+            The Euclidean distance between the points or 
+            perpendicular distance from point to line
         """
-        return math.sqrt((other.x - self.x) ** 2 + (other.y - self.y) ** 2)
+        if hasattr(other, 'distance_to_point'):
+            # It's a Line object
+            return other.distance_to_point(self)
+        else:
+            # It's a Point object
+            return math.sqrt((other.x - self.x) ** 2 + (other.y - self.y) ** 2)
     
     def __repr__(self) -> str:
         return f"Point({self.x}, {self.y})"
